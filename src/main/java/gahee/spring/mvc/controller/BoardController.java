9112b36e8579435948ed5aa6606a11e19f93e86c
gahee <rkgml284@gmail.com>
2021-06-07 15:44:51 +0900

    SemiProjectV3 기본 설정

---
+++ src/main/java/gahee/spring/mvc/controller/BoardController.java
@@ -0,0 +1,24 @@
+package gahee.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/list")
+    public String list(){
+        return "board/list.tiles";
+    }
+
+    @GetMapping("/board/view")
+    public String view(){
+        return "board/view.tiles";
+    }
+
+    @GetMapping("/board/write")
+    public String write(){
+        return "board/write.tiles";
+    }
+
+}
